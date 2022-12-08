198323935eb8b219886e29f1840f6f623dbf383d
YS739 <suk921@gmail.com>
2022-12-09 08:38:44 +0900

    clone coding udemy

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <h4>{expenseDate.toISOString()}</h4>
+      <div className="expense-item_description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item_price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
