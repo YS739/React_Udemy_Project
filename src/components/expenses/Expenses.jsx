224f2bda6e19bbe7ebdb3085b304bbef2ca58505
YS739 <suk921@gmail.com>
2022-12-09 14:08:18 +0900

    done homework by myself

--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,7 +1,31 @@
 import "./Expenses.css";
+import ExpenseItem from "../Item/ExpenseItem";
 
-function Expenses(props) {
-  return;
+function Expenses({ expenses }) {
+  return (
+    <div>
+      <ExpenseItem
+        name={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        name={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        name={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        name={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
 }
 
 export default Expenses;
