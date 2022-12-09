c5cfa49577acb3a79730308b4a49fc3a4cb5c9c1
YS739 <suk921@gmail.com>
2022-12-09 12:35:24 +0900

    error toLocaleString undefined while makind component in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>
-        <div>Month</div>
-        <div>Year</div>
-        <div>Day</div>
-      </div>
+      <ExpenseDate />
       <div className="expense-item_description">
         <h2>{props.name}</h2>
         <div className="expense-item_price">${props.amount}</div>
