988dc3a4f22e819183b88c9f9caabb1361238547
YS739 <suk921@gmail.com>
2022-12-09 11:34:27 +0900

    start props! used props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
-      <h4>{expenseDate.toISOString()}</h4>
+      <h4>{props.date.toISOString()}</h4>
       <div className="expense-item_description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item_price">${expenseAmount}</div>
+        <h2>{props.name}</h2>
+        <div className="expense-item_price">${props.amount}</div>
       </div>
     </div>
   );
